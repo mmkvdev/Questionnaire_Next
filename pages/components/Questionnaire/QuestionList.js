5c7554ef5f3a0c40a505a7c81e6279a40789c7df
Madhu5A3 <madhuv299@gmail.com>
2020-06-09 16:52:38 +0530

    Update: Edit QuestionListComponent Updated

--- pages/components/Questionnaire/QuestionList.js
+++ pages/components/Questionnaire/QuestionList.js
@@ -1,28 +1,9 @@
 import { useSelector } from 'react-redux';
 import Router from 'next/router';
 import Layout from './Layout';
-// import { useHistory } from 'react-router-dom';
-
-/*
-    export const disQuestionListWrapper = css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 14px 100px;
-    `;
-
-    export const buttonListContainer = css`
-    width: 100px;
-    margin: 10px;
-    text-align: center;
-    padding: 10px;
-    border-radius: 2px;
-    `;
-*/
 
 const QuestionList = () => {
     const questionList = useSelector(state => state.QuestionnaireReducer.questions);
-    // console.table(questionList.questions)
     const disQuestions = questionList.map(question => {
         return (
             <div>
