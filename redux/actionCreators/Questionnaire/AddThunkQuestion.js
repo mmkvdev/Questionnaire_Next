a5c3f3fa53ecbb3fb31062080caade0e5424fdef
Madhu5A3 <madhuv299@gmail.com>
2020-06-09 18:48:47 +0530

    Update: Questionnaire AddThunk APi Updated

--- redux/actionCreators/Questionnaire/AddThunkQuestion.js
+++ redux/actionCreators/Questionnaire/AddThunkQuestion.js
@@ -2,7 +2,7 @@ import { addQuestions } from './AddQuestion';
 
 const AddThunkQuestion = () => {
     return (dispatch) => {
-        fetch('http://localhost:3000/api/questionnaire')
+        fetch('/api/questionnaire')
             .then(res => res.json())
             .then(res => { dispatch(addQuestions(res)) })
     };
