54810c63632711609573449ad37d7374feb3075e
Madhu Kiran Varma <madhuv299@gmail.com>
2020-06-15 08:58:24 +0530

    Middleware Database Logic Updated

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,15 +1,21 @@
-import mongoose from 'mongoose';
-const connection = {};
-async function dbConnect() {
-    if(connection.isConnected){
-        return;
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+export async function setUp(db) {
+    db.collection('users').createIndex({ email: 1 }, { unique: true });
+}
+async function database(req, res, next) {
+    if (!client.isConnected()) {
+        await client.connect()
     }
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true
-    });
-    connection.isConnected = db.connections[0].readyState;
-    console.log(connection.isConnected)
+    req.dbClient = client
+    req.db = client.db(process.env.DB_NAME);
+    await setUp(req.db);
+    return next();
 }
 
-export default dbConnect;
+export default database;
